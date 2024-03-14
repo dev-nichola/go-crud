5c27d159ef94ebb553d33d479d3c0a106418d898
dev-nichola <kerjasamakol@gmail.com>
2024-03-14 21:57:35 +0700

    create employe

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,16 @@
 package routes
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/dev-nichola/go-crud/controller"
 )
 
-func MapRoutes(server *http.ServeMux) {
+func MapRoutes(server *http.ServeMux, db *sql.DB) {
 	server.HandleFunc("/", controller.NewHelloWorldController())
+
+	server.HandleFunc("/employe", controller.NewIndexEmployeController())
+	server.HandleFunc("/employe/create", controller.NewCreateEmployeController(db))
 
 }
